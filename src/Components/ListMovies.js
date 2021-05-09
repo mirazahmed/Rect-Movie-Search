56ced9e31e1b0cb648f8341382da52116a16f91b
Miraz Ahmed <miraz37@gmail.com>
2021-05-08 20:08:10 -0400

    Bug fixing

--- src/Components/ListMovies.js
+++ src/Components/ListMovies.js
@@ -1,21 +1,29 @@
- import React,{useState} from 'react';
+
+//This component takes props from MovieSearchForm and-
+//List all the movies with title, year and Nominate button
+//Save the movies selected for nominations in nominatedMovies state
+//Create a function to remove a movie from the NominationList
+//Render nominatedMovies and removeMovie as props to NominationList component
+
+import React,{useState} from 'react';
 import { Button, Card,Row, Col} from 'react-bootstrap';
 import NominationList from './NominationList';
 
-
+let Spinner = require('react-spinkit');
 
 export default function ListMovies(props) {
 
     const [nominatedMovies,setNominatedMovie] = useState([]);         
 
-   const addNewMovie = (movie)=>{
+    const addNewMovie = (movie)=>{
 
           setNominatedMovie(
-            [...nominatedMovies, {Title: movie.Title, Year: movie.Year}]            
+            [...nominatedMovies, {Title: movie.Title, Year: movie.Year, movieID: movie.imdbID}]            
         )                
                 
     }
 
+    
     const removeMovie = (index)=>{
         const copyOfNominatedMovies = [...nominatedMovies];
         copyOfNominatedMovies.splice(index,1)
@@ -27,13 +35,23 @@ export default function ListMovies(props) {
     const movies = props.movieList;
     
     const movieListHeading = (
+
         <div>
-            <h3 id="movieListHeading">Results for "{props.movieTitle}"</h3>
+            <h3>Results for "{props.movieTitle}"</h3>
+        
+            {props.movieTitle==='' ? (
+            <div>
+                <span>Waiting for user to search for a movie </span> 
+                <Spinner name="three-bounce" className ="waitingforTitle" fadeIn='quarter' color="purple"/>            
+            </div>) : (<div></div>)}
+
         </div>
+        
     )
 
-    const allMovies = (movies.movieList === '' || movies.movieList === undefined ? ( <div> No movies found...</div>) :           
-    ( 
+    const allMovies = ((movies.movieList === '' || movies.movieList === undefined) ? 
+    (<div></div>) : ( 
+        
         movies.movieList.map((movie,index)=>{
             
         return(
@@ -44,7 +62,7 @@ export default function ListMovies(props) {
                     <span className="movieTitle">{movie.Title}</span> 
                     <span className="movieYear">({movie.Year})</span> 
                     {                            
-                        (nominatedMovies.find(nominatedMovie=> nominatedMovie.Title === movie.Title) !== undefined) ? 
+                        (nominatedMovies.find(nominatedMovie=> nominatedMovie.movieID === movie.imdbID ) !== undefined) ? 
                         
                         (<Button variant="secondary" disabled>Nominate</Button>) :
 
@@ -59,34 +77,29 @@ export default function ListMovies(props) {
         )
     })))
 
-   return (  
+    return(  
+                 
+        <div className="container fluid">
+            <Row>
+                <Col>
+                    <Card className="movieListCard">
+                        <Card.Body>
+                            <div>{movieListHeading}</div>
+                            <div>{allMovies}</div>  
+                        </Card.Body>
+                    </Card>
+                </Col>
 
-        (props.movieTitle === '' ? (<div></div>):
-                
-        
-            <div className="container fluid">
-                
-                <Row>
-                    <Col>
-                        <Card className="movieListCard">
-                            <Card.Body>
-                                <div>{movieListHeading}</div>
-                                <div>{allMovies}</div>  
-                            </Card.Body>
-                        </Card>
-                    </Col>
-
-                    <Col>
-                        <Card className="nominationListCard">
-                            <Card.Body>
-                                <NominationList nominatedMovies={nominatedMovies} removeMovie={removeMovie}/>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>                                                
-                    
-        )         
+                <Col>
+                    <Card className="nominationListCard">
+                        <Card.Body>
+                            <NominationList nominatedMovies={nominatedMovies} removeMovie={removeMovie}/>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </div>                                                           
+         
     )
 }
 
